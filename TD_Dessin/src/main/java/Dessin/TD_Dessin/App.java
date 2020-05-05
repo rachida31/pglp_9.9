f8fb8b1d370b5f52bca9ff9adf41133e86e47628
Rachida <rachidaouchene1@gmail.com>
2020-05-05 13:47:16 +0200

    pom

---
+++ TD_Dessin/src/main/java/Dessin/TD_Dessin/App.java
@@ -0,0 +1,13 @@
+package Dessin.TD_Dessin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
